254ff2815ac160c56320f92c72e8d517bee3b999
jkalasas <alasasjohnkyle@gmail.com>
2025-11-01 14:59:20 +0800

    refactor: used svelte env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,21 @@
 import { initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAI, type AI, GoogleAIBackend } from 'firebase/ai';
+import {
+	PUBLIC_FIREBASE_API_KEY,
+	PUBLIC_FIREBASE_AUTH_DOMAIN,
+	PUBLIC_FIREBASE_PROJECT_ID,
+	PUBLIC_FIREBASE_STORAGE_BUCKET,
+	PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	PUBLIC_FIREBASE_APP_ID
+} from '$env/static/public';
 
 const firebaseConfig = {
-	apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
-	authDomain: import.meta.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.PUBLIC_FIREBASE_APP_ID
+	apiKey: PUBLIC_FIREBASE_API_KEY,
+	authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: PUBLIC_FIREBASE_APP_ID
 };
 
 let app: FirebaseApp | null = null;
